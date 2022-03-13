c1db52ec8b8c366d5ec2257f9779769e78f7f72e
ayaz1206 <89700556+ayaz1206@users.noreply.github.com>
2022-03-13 19:01:22 +0530

    Add files via upload

---
+++ pygame.py
@@ -0,0 +1,8 @@
+import pygame
+background_colour = (255,255,255)
+(width, height) = (300, 200)
+screen = pygame. display. set_mode((width, height))
+pygame. display. set_caption('Tutorial 1')
+screen. fill(background_colour)
+pygame. display. flip()
+running = True
